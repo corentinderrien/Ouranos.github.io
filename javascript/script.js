d411d56446474680b7f137ffa0e967fc28f4a060
corentinderrien <58248785+corentinderrien@users.noreply.github.com>
2020-12-30 20:19:58 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,13 @@
 function menu() {
-        document.querySelector("nav").classList.toggle("ouvert");
-        document.querySelector("#hamburger-button").classList.toggle("burger2");
-    }
+    document.querySelector("nav").classList.toggle("ouvert");
+    document.querySelector("#hamburger-button").classList.toggle("burger2");
+}
+
+
+function moveCible(event){
+    var e = document.getElementById("Cible");
+    e.style.transform = "translate("+event.clientX+"px, "+event.clientY+"px)";
+    e.classList.remove("anim_Cible");
+    e.offsetWidth;
+    e.classList.add("anim_Cible");
+}
